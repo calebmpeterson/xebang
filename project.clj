74ace44f6efa9089ffdaf14f866b2136fd2d598e
Caleb Peterson <caleb.m.peterson@gmail.com>
2013-10-21 23:17:19 -0500

    added ring-jetty-adapter dependency to project.clj

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.2.0"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.3"]]
   :plugins [[lein-ring "0.8.5"]]
