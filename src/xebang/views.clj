3a1cf9825bb9bcbc746d91dd093ac3608db09ef5
Caleb Peterson <caleb.m.peterson@gmail.com>
2013-10-22 23:04:13 -0500

    adjusted the visual style

--- src/xebang/views.clj
+++ src/xebang/views.clj
@@ -5,24 +5,37 @@
   (html5
    [:head
     [:title "Xebang! - Custom URL Bangs"]
-    (include-css "/libs/bootstrap/css/bootstrap.cyborg.css")
+    (include-css "/libs/bootstrap/css/bootstrap.spacelab.min.css")
     (include-css "/css/main.css")
     (include-js "/libs/jquery/jquery-1.9.1.min.js")
     (include-js "/libs/bootstrap/js/bootstrap.min.js")
     (include-js "/js/script.js")]
    [:body
-    [:div.container
-     [:div {:style "width: 100%; height: 300px;"}]
-     [:div.row
-      [:div.span6.offset3
-       [:h1 "xebang!" [:small " custom url bangs"]]]]
-     [:div.row
-      [:div.span6.offset3
-       [:form.form-inline {:id "command" :method "GET" :action "/q/"}
-        [:div.input-append
-         [:input.span5 {:id "input" :name "query" :type "text" :placeholder "bang bang!" :autofocus true}]
-         [:button.btn.btn-primary [:i.icon-search.icon-white]]]]]]
-     [:div.row
-      [:div.span6.offset3 "Enter a bang. Perform a search. "
-       [:a {:href "/help"} "View the docs"] ". "
-       [:a {:href "/browser"} "Tell your browser"] "!"]]]]))
+    [:div#wrapper
+     [:div.container
+      [:div {:style "width: 100%; height: 300px;"}]
+      [:div.row
+       [:div.span6.offset3
+        [:h1 "xebang!" [:small " custom url bangs"]]]]
+      [:div.row
+       [:div.span6.offset3
+        [:form.form-inline {:id "command" :method "GET" :action "/q/"}
+         [:div.input-append
+          [:input.span5 {:id "input" :name "query" :type "text" :placeholder "bang bang!" :autofocus true}]
+          [:button.btn.btn-primary [:i.icon-search.icon-white]]]]]]
+      [:div.row
+       [:div.span6.offset3
+        [:small "Enter a bang. Perform a search. "
+         [:a {:href "/help"} "View the docs"] ". "
+         [:a {:href "/browser"} "Tell your browser"] "!"]]]]
+     [:div.push]]
+    [:div.footer-top-trim]
+    [:footer
+     [:div.container
+      [:div.spacer20]
+      [:div.row
+        [:div.span6.offset3.text-center
+         [:small "Built by "
+          [:a {:href "http://www.cubicle6.com"} "Caleb Peterson"] " | "
+          [:a {:href "mailto:caleb.peterson@cubicle6.com"} "E-mail Me"] " | "
+          "Soli Deo Gloria"]]]]]]))
