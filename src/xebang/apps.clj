76e46d288828180cdc1567165d06e7ba04f99c1d
Caleb Peterson <caleb.m.peterson@gmail.com>
2013-10-30 17:22:58 -0500

    switched from Weather.com to WeatherSpark.com for !weather

--- src/xebang/apps.clj
+++ src/xebang/apps.clj
@@ -20,7 +20,7 @@
 
 (def catalog
   {'!bible   "http://www.bible.com/search?q={{{s}}}"
-   '!weather "http://www.weather.com/weather/hourbyhour/graph/{{{s}}}"
+   '!weather "http://weatherspark.com/#!dashboard;q={{{s}}}"
    '!devdocs "http://devdocs.io/#q={{{s}}}"
    '!js      "http://devdocs.io/#q=js {{{s}}}"
    '!css     "http://devdocs.io/#q=css {{{s}}}"
