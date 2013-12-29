a76b05b42846b42eeba54401553ce9696e231654
Caleb Peterson <caleb.m.peterson@gmail.com>
2013-12-29 15:07:18 -0600

    new bangs

--- src/xebang/models/bangs.clj
+++ src/xebang/models/bangs.clj
@@ -1,11 +1,11 @@
-(ns xebang.models.bangs
-  (:require [xebang.models.db :as db]
-            [somnium.congomongo :as m]))
+(ns xebang.models.bangs)
 
 (def catalog
   {'!bible      "http://www.bible.com/search?q={{{s}}}"
    '!kidsinmind "http://www.kids-in-mind.com/cgi-bin/search/search.pl?q={{{s}}}"
    '!weather    "http://weatherspark.com/#!dashboard;q={{{s}}}"
+   '!w          "http://weatherspark.com/#!dashboard;q={{{s}}}"
+   '!syn        "http://thesaurus.com/browse/{{{s}}}"
    '!devdocs    "http://devdocs.io/#q={{{s}}}"
    '!js         "http://devdocs.io/#q=js {{{s}}}"
    '!css        "http://devdocs.io/#q=css {{{s}}}"
@@ -23,6 +23,3 @@
 				   {:bang "!html"       :name "HTML"         :url-pattern "http://devdocs.io/#q=html {{{s}}}"}
 				   {:bang "!namecheap"  :name "Domain Search"
 				    :url-pattern "http://www.namecheap.com/domains/domain-name-search/results.aspx?domain={{{s}}}&tlds=&searchall=&type=single&"}]})
-
-(when-not (m/fetch-count :config)
-  (m/insert! :config))
